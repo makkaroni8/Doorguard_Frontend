0375a66460d81f4b1fd092fd7d7342a8cccac865
qeinz <66924611+qeinz@users.noreply.github.com>
2024-03-29 16:15:46 +0100

    darkmode theme service not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,15 @@ import {AuthService} from "./services/authservice";
 import {NgClass, NgIf} from "@angular/common";
 import {AccountSettingsDialogComponent} from "./dialogs/account-settings-dialog/account-settings-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {ThemeService} from "./services/themeService";
+import {inject} from "@angular/core/testing";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, RouterLink, MatMenuTrigger, MatMenu, MatMenuItem, NgIf, NgClass],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, RouterLink, MatMenuTrigger, MatMenu, MatMenuItem, NgIf, NgClass, MatSlideToggle, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,11 +24,14 @@ export class AppComponent {
   title = 'doorguadFrontend';
   loggedIn: boolean = false;
   accountToken: string | null = null;
-
+  isChecked: boolean | undefined;
 
   constructor(protected authService: AuthService,
               private router: Router,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              protected themeService: ThemeService) {
+
+    this.isChecked = this.themeService.themeSignal() === 'dark'
 
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
@@ -60,6 +67,10 @@ export class AppComponent {
 
   dashboard() {
     this.router.navigate(['/admin-page']);
+  }
+
+  toggleChanged(): void {
+    this.themeService.updateTheme();
   }
 
 
