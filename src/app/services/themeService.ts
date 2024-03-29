c6e230c934ac4386be8d10627fe945041c74f101
qeinz <66924611+qeinz@users.noreply.github.com>
2024-03-29 17:44:59 +0100

    added save theme

--- src/app/services/themeService.ts
+++ src/app/services/themeService.ts
@@ -4,13 +4,17 @@ import {Injectable, signal} from "@angular/core";
   providedIn: 'root'
 })
 export class ThemeService {
-  themeSignal = signal<string>("dark");
+  private readonly THEME_KEY = 'app_theme';
+
+  themeSignal = signal<string>(localStorage.getItem(this.THEME_KEY) || 'light');
 
   setTheme(theme: string) {
-    this.themeSignal.set(theme)
+    this.themeSignal.set(theme);
+    localStorage.setItem(this.THEME_KEY, theme);
   }
 
   updateTheme() {
     this.themeSignal.update((value) => (value === "dark" ? "light" : "dark"));
+    localStorage.setItem(this.THEME_KEY, this.themeSignal());
   }
 }
